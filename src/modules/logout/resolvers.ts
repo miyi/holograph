6e018dd46873736f637ee74be07a8d0389030f0b
miyi <mylegojesus@gmail.com>
2020-08-06 23:44:44 -0700

    uses lrem and session persists

--- src/modules/logout/resolvers.ts
+++ src/modules/logout/resolvers.ts
@@ -6,17 +6,24 @@ let authError: AuthError
 
 export const resolvers: ResolverMap = {
 	Mutation: {
-		logout: (_, __, { session }) => {
+		logout: async (_, __, { session, redis }) => {
 			let error: Array<AuthError> = []
 			let success: boolean = false
-			if (!session.userId) {
+			let sessionId: string
+			let userId: string
+			if (session && !session.userId) {
 				authError = {
 					path: 'session',
 					message: 'no user found',
 				}
 				error.push(authError)
+			} else {
+				sessionId = session.id
+				userId = session.userId
+				let rep: string = await redis('lrem', [userId, -1, sessionId])
+				console.log(rep);
 			}
-			return new Promise((res) => session.destroy((err: string) => {
+			return new Promise((res) => session.destroy(async (err: string) => {
 				if (!err) {
 					success = true
 				} else {
