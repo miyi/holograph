847d65806f0b57836be0a2879e29f9021155d8fe
miyi <mylegojesus@gmail.com>
2020-09-24 14:52:45 -0700

    typos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import 'reflect-metadata'
-// import {createConnection} from "typeorm";
-// import {User} from "./entity/User";
 require('dotenv').config()
 import { startServer } from './startServer'
+
 startServer()
