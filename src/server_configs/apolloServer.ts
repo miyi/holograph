c5ba72c134ba4e1c52979feed414ee32e0931f3f
miyi <mylegojesus@gmail.com>
2020-10-11 22:55:00 -0700

    merge issues

--- src/server_configs/apolloServer.ts
+++ src/server_configs/apolloServer.ts
@@ -1,13 +1,8 @@
 import { asyncRedis } from './redisServer'
 import { ContextIntegration } from '../types/server-utils'
 import { ApolloServer } from 'apollo-server-express'
-<<<<<<< HEAD
-import { prepareGQLDocuments } from '../utils/prepareGQLDocuments'
-import { Express } from 'express'
-=======
 import { Express } from 'express'
 import { prepareGQLDocuments } from './prepareGQLDocs'
->>>>>>> socialauth
 
 const initApolloServer = (app: Express) => {
   const { resolvers, typeDefs } = prepareGQLDocuments()
@@ -33,8 +28,5 @@ const initApolloServer = (app: Express) => {
 	})
 }
 
-<<<<<<< HEAD
 export { initApolloServer }
-=======
-export { initApolloServer }
->>>>>>> socialauth
+
