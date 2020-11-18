0344a9158eb75ebc35deff559d87a8dee2411146
miyi <mylegojesus@gmail.com>
2020-11-18 14:52:18 -0800

    updating

--- src/modules/profiles/profileCore.test.ts
+++ src/modules/profiles/profileCore.test.ts
@@ -27,8 +27,8 @@ beforeAll(async () => {
 })
 
 afterAll(async () => {
-  await server.close()
-  await new Promise((resolve) => {
+  server.close()
+  new Promise((resolve) => {
     redis.quit(() => {
       resolve()
     })
